898d2bab0cf5bbaba7a9c70bb182d05ec06ea778
edux1 <edu.pm97@gmail.com>
2023-02-07 20:16:08 +0100

    solutions

---
+++ Javascript/0206_Reverse_Linked_List.js
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if(!head || !head.next)
+        return head
+
+    let cur = reverseList(head.next)
+    head.next.next = head
+    head.next = null
+    
+    return cur
+};
