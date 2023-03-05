eab71810ca9360401ff7d4dad3967e72c2074205
edux1 <edu.pm97@gmail.com>
2023-03-06 00:27:28 +0100

    solutions

--- Python/0209_Minimum_Size_Subarray_Sum.py
+++ Python/0209_Minimum_Size_Subarray_Sum.py
@@ -19,7 +19,6 @@ class Solution(object):
                 end += 1
         while start < n and sum >= target:
             minimum = min(minimum, end - start)
-            print(minimum)
             sum -= nums[start]
             start += 1
         if minimum == n+1:
