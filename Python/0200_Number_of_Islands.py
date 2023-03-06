ec49d1cb577bc40d35dbc653d8a24fb1dfd6ff0b
edux1 <edu.pm97@gmail.com>
2023-03-06 17:33:24 +0100

    solutions

---
+++ Python/0200_Number_of_Islands.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def numIslands(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        def dfs(i, j):
+            if i < 0 or i >= m or j < 0 or j >= n or grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            dfs(i-1, j)
+            dfs(i+1, j)
+            dfs(i, j-1)
+            dfs(i, j+1)
+
+        visited, queue = [], []
+        m, n = len(grid), len(grid[0])
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    print((i, j))
+                    count += 1
+                    dfs(i,j)
+
+        return count
