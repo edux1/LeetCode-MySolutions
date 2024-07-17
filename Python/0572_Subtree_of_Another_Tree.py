fa89f7ba38ed3b7b033000707e40aa2db55cef33
edux1 <edu.pm97@gmail.com>
2024-07-18 00:41:27 +0200

    subtree of another three

---
+++ Python/0572_Subtree_of_Another_Tree.py
@@ -0,0 +1,39 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSubtree(self, root, subRoot):
+        """
+        :type root: TreeNode
+        :type subRoot: TreeNode
+        :rtype: bool
+        """
+        # Time Complexity: O(M*N)
+        if root is not None:
+            print("==")
+            print(root.val)
+        if root == None:
+            return False
+        elif self.isSameTree(root, subRoot):
+            return True
+        else:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+    
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        # Time Complexity: O(N)
+        if p == None and q == None:
+            return True
+        elif p == None or q == None:
+            return False
+        elif p.val != q.val:
+            return False
+        else:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
