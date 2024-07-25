e78633f69dd08d8b31ddf30668f5d85d290c9268
edux1 <edu.pm97@gmail.com>
2024-07-26 01:23:23 +0200

    Permutations II

---
+++ Python/0047_Permutations_II.py
@@ -0,0 +1,33 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        # Time Complexity: O(N*N!)
+        n = len(nums)
+        nums.sort()
+
+        def permute(permutations, permutation, nums):
+            n = len(nums)
+            if n == 0:
+                permutations.append(permutation)
+                return permutations
+            
+            for i in range(n):
+                new_list = list(permutation)
+                new_list.append(nums[i])
+                reduced_list = list(nums)
+                reduced_list.pop(i)
+                if i == 0 or (i > 0 and nums[i] != nums[i-1]):
+                    permutations = permute(permutations, new_list, reduced_list)
+            return permutations
+
+        permutations = []
+        for i in range(n):
+            reduced_list = list(nums)
+            reduced_list.pop(i)
+            if i == 0 or (i > 0 and nums[i] != nums[i-1]):
+                permutations = permute(permutations, [nums[i]], reduced_list)
+        return permutations
+        
