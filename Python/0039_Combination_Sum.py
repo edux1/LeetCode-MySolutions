6851cd913a148e19eff49ac96a50047b1456ca76
edux1 <edu.pm97@gmail.com>
2024-07-27 00:58:09 +0200

    Improvement Combination Sum

--- Python/0039_Combination_Sum.py
+++ Python/0039_Combination_Sum.py
@@ -6,28 +6,21 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         # Time Complexity: O(2^N)
-        def getCombinations(i, sum_value, combination):
-            candidate = candidates[i]
-
-            sum_value += candidate
+        def getCombinations(start, sum_value, combination):
+            if sum_value == target:
+                combinations.append(list(combination))
+                return
+            
             if sum_value > target:
                 return
             
-            combination.append(candidate)
-            
-            if sum_value == target:
-                print(combination)
-                combinations.append(combination)
-                return
-            
-            for j in range(i, len(candidates)):
-                getCombinations(j, sum_value, list(combination))
-                
-
+            for i in range(start, len(candidates)):
+                candidate = candidates[i]
+                combination.append(candidate)
+                getCombinations(i, sum_value + candidate, combination)
+                combination.pop()
+        
         candidates.sort()
         combinations = []
-
-        for i in range(len(candidates)):
-            getCombinations(i, 0, [])
-
+        getCombinations(0, 0, [])
         return combinations
