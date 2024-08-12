bd733bddd8b1c66bbd2c6902a12ece4b6bf29361
edux1 <edu.pm97@gmail.com>
2024-08-12 20:19:34 +0200

    Jump Game

--- Python/0055_Jump_Game.py
+++ Python/0055_Jump_Game.py
@@ -4,7 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        # Time Complexity: O(N)
+        # Time Complexity O(N)
         n = len(nums)
 
         if n == 1 or nums[0] >= n-1:
@@ -12,13 +12,11 @@ class Solution(object):
         if n == 2:
             return False
 
-        pos = n-1
         distance_to_pos = 0
 
         for i in range(n-2, -1, -1):
             distance_to_pos += 1
             if nums[i] >= distance_to_pos:
-                pos = i
                 distance_to_pos = 0
             
         if distance_to_pos == 0:
