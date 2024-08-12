28fd62230d8e7cbb768b998db5a2c234b12bacef
edux1 <edu.pm97@gmail.com>
2024-08-12 20:16:52 +0200

    Jump Game

---
+++ Python/0055_Jump_Game.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def canJump(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        # Time Complexity: O(N)
+        n = len(nums)
+
+        if n == 1 or nums[0] >= n-1:
+            return True
+        if n == 2:
+            return False
+
+        pos = n-1
+        distance_to_pos = 0
+
+        for i in range(n-2, -1, -1):
+            distance_to_pos += 1
+            if nums[i] >= distance_to_pos:
+                pos = i
+                distance_to_pos = 0
+            
+        if distance_to_pos == 0:
+            return True
+        return False
