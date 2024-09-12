4bd6e899ea9c9f375d60e618178ff639caa7f2d2
edux1 <edu.pm97@gmail.com>
2024-09-12 12:41:31 +0200

    Add Two Integers

---
+++ Python/2235_Add_Two_Integers.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        # Time Complexity: O(1)
+        # Space Complexity: O(1)
+        return num1 + num2
+        
