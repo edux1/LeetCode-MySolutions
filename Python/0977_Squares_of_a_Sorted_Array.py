b5220f8d7a6669bd67c6b1c6e2f1c7664c396080
edux1 <edu.pm97@gmail.com>
2024-09-12 23:01:30 +0200

    Squares of a Sorted Array

--- Python/0977_Squares_of_a_Sorted_Array.py
+++ Python/0977_Squares_of_a_Sorted_Array.py
@@ -4,13 +4,24 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[int]
         """
-        l, r = 0, len(nums) -1
-        list = []
-        while l <= r:
-            if abs(nums[l]) <= abs(nums[r]):
-                list.insert(0,nums[r] ** 2)   
-                r -= 1
-            else:
-                list.insert(0,nums[l] ** 2 ) 
+        # Time Complexity: O(N)
+        # Space Complexity: O(N)
+        n = len(nums)
+        l, r = 0, n-1
+        sorted_squares = [0] * n
+        i = n-1
+        
+        while l < r:
+            if abs(nums[l]) >= abs(nums[r]):
+                sorted_squares[i] = nums[l]**2
                 l += 1
-        return list
+            else:
+                sorted_squares[i] = nums[r]**2
+                r -= 1
+            i -= 1
+        
+        if l == r:
+            sorted_squares[0] = nums[l]**2
+        
+        return sorted_squares
+            
