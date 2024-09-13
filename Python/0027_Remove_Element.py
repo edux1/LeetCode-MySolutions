e1053981303a5777c16abfcb5a1f24efcca9073e
edux1 <edu.pm97@gmail.com>
2024-09-13 17:30:37 +0200

    Remove Element

---
+++ Python/0027_Remove_Element.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        # Time Complexity: O(N)
+        # Space Complexity: O(1)
+        i = 0
+        for j in range(len(nums)):
+            if nums[j] != val:
+                nums[i] = nums[j]
+                i += 1
+        
+        return i
