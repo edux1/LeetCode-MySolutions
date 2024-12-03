cef65391cda1b06820735339234103ae794193f1
edux1 <edu.pm97@gmail.com>
2024-12-03 21:39:59 +0100

    Minimum Size Subarray Sum

--- Python/0209_Minimum_Size_Subarray_Sum.py
+++ Python/0209_Minimum_Size_Subarray_Sum.py
@@ -5,22 +5,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        # Time complexity: O(n)
+        # Space complexity: O(1)
+
         n = len(nums)
-        start, end = 0, 0
-        sum = 0
-        minimum = n+1
-        while end < n:
-            if sum >= target:
-                minimum = min(minimum, end - start)
-                sum -= nums[start]
-                start += 1
-            else:
-                sum += nums[end]
-                end += 1
-        while start < n and sum >= target:
-            minimum = min(minimum, end - start)
-            sum -= nums[start]
-            start += 1
-        if minimum == n+1:
-            return 0
-        return minimum
+        left = 0
+        min_length = n+1
+        
+        for right in range(n):
+            target -= nums[right]
+
+            while target <= 0:
+                min_length = min(min_length, right - left + 1)
+                target += nums[left]
+                left += 1
+        
+        return min_length if min_length <= n else 0
